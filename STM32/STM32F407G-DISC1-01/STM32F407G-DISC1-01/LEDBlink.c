cbbb027f9b1c40ae8803cb4ed3f913e112286fdc
Hugh <hugh_gleaves@hotmail.com>
2020-01-05 11:15:26 -0700

    Simple helpers to ease readability.

--- STM32/STM32F407G-DISC1-01/STM32F407G-DISC1-01/LEDBlink.c
+++ STM32/STM32F407G-DISC1-01/STM32F407G-DISC1-01/LEDBlink.c
@@ -10,6 +10,15 @@ void SysTick_Handler(void)
 	HAL_SYSTICK_IRQHandler();
 }
 
+#define GREEN GPIO_PIN_12
+#define ORANGE GPIO_PIN_13
+#define RED GPIO_PIN_14
+#define BLUE GPIO_PIN_15
+
+void LED_ON(uint16_t ID);
+void LED_OFF(uint16_t ID);
+
+
 int main(void)
 {
 	HAL_Init();
@@ -33,40 +42,50 @@ int main(void)
 	GPIO_InitStructure.Pin = GPIO_PIN_15;
 	HAL_GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-
 	for (;;)
 	{
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+		LED_ON(GREEN);
+		LED_ON(ORANGE);
+		LED_ON(RED);
+		LED_ON(BLUE);
+		
 		HAL_Delay(100);
 		
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+		LED_OFF(GREEN);
+		LED_OFF(ORANGE);
+		LED_OFF(RED);
+		LED_OFF(BLUE);
+		
 		HAL_Delay(100);
 		
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+		LED_ON(GREEN);
 		HAL_Delay(100);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+		LED_OFF(GREEN);
 		HAL_Delay(100);
 		
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+		LED_ON(ORANGE);
 		HAL_Delay(100);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
-		HAL_Delay(100);
-
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-		HAL_Delay(100);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+		LED_OFF(ORANGE);
 		HAL_Delay(100);
 
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+		LED_ON(RED);
 		HAL_Delay(100);
-		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+		LED_OFF(RED);
 		HAL_Delay(100);
 
+		LED_ON(BLUE);
+		HAL_Delay(100);
+		LED_OFF(BLUE);
+		HAL_Delay(100);
 	}
+}
+
+void LED_ON(uint16_t ID)
+{
+	HAL_GPIO_WritePin(GPIOD, ID, GPIO_PIN_SET);
+}
+
+void LED_OFF(uint16_t ID)
+{
+	HAL_GPIO_WritePin(GPIOD, ID, GPIO_PIN_RESET);
 }
