a59afcd10972c295bcd2735e62be572bd58e0364
Hugh <hugh_gleaves@hotmail.com>
2020-01-18 08:58:39 -0700

    Unroll simple loop to see how scope looks.

--- STM32/STM32F407G-DISC1-TIMERS/STM32F407G-DISC1-TIMERS.c
+++ STM32/STM32F407G-DISC1-TIMERS/STM32F407G-DISC1-TIMERS.c
@@ -1,11 +1,17 @@
 #include <stm32f4xx_ll_bus.h>
 #include <stm32f4xx_ll_gpio.h>
 #include <stm32f4xx_ll_utils.h>
+//#include <stm32f4xx_rcc.h>
+	
 
 int main(void)
 {
 	LL_InitTick(16000000, 1000);
 
+	//RCC_ClocksTypeDef def;
+	
+	//RCC_GetClocksFreq(&def);
+	
 	//Warning: if the line below triggers an error, GPIOC is not connected to a AHB1 (Group 1) on this device.
 	//In this case, please search the stm32xxxx_ll_bus.h file for 'PERIPH_GPIOC' to find out the correct
 	//macro name and use it to replace LL_AHB1_GRP1_PERIPH_$$com.sysprogs.examples.lBedblink.LEDPORT$$ and LL_AHB1_GRP1_EnableClock() below. 
@@ -14,11 +20,29 @@ int main(void)
 	LL_GPIO_SetPinOutputType(GPIOD, LL_GPIO_PIN_15, LL_GPIO_OUTPUT_PUSHPULL);
 	LL_GPIO_SetPinSpeed(GPIOD, LL_GPIO_PIN_15, LL_GPIO_SPEED_FREQ_LOW);
 
+	uint32_t ddd = SystemCoreClock;
+		
 	for (;;)
 	{
-		LL_GPIO_SetOutputPin(GPIOD, LL_GPIO_PIN_15);
-		LL_mDelay(1);
-		LL_GPIO_ResetOutputPin(GPIOD, LL_GPIO_PIN_15);
-		LL_mDelay(1);
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
+		WRITE_REG(GPIOD->BSRR, LL_GPIO_PIN_15);
+		WRITE_REG(GPIOD->BSRR, (LL_GPIO_PIN_15 << 16));
 	}
 }
