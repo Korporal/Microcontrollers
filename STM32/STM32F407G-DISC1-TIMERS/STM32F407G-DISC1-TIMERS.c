011a2ad6f241bc19c38bbb988bfa2d335e53202a
Hugh <hugh_gleaves@hotmail.com>
2020-01-18 09:00:49 -0700

    Remove stuff that we need to understand better.

--- STM32/STM32F407G-DISC1-TIMERS/STM32F407G-DISC1-TIMERS.c
+++ STM32/STM32F407G-DISC1-TIMERS/STM32F407G-DISC1-TIMERS.c
@@ -1,17 +1,12 @@
 #include <stm32f4xx_ll_bus.h>
 #include <stm32f4xx_ll_gpio.h>
 #include <stm32f4xx_ll_utils.h>
-//#include <stm32f4xx_rcc.h>
 	
 
 int main(void)
 {
 	LL_InitTick(16000000, 1000);
 
-	//RCC_ClocksTypeDef def;
-	
-	//RCC_GetClocksFreq(&def);
-	
 	//Warning: if the line below triggers an error, GPIOC is not connected to a AHB1 (Group 1) on this device.
 	//In this case, please search the stm32xxxx_ll_bus.h file for 'PERIPH_GPIOC' to find out the correct
 	//macro name and use it to replace LL_AHB1_GRP1_PERIPH_$$com.sysprogs.examples.lBedblink.LEDPORT$$ and LL_AHB1_GRP1_EnableClock() below. 
