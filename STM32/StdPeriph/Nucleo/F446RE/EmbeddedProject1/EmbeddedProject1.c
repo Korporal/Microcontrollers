44b4ec2ab67fb5a8e195790f92a0513b218a1cbd
Hugh <hugh_gleaves@hotmail.com>
2020-02-23 11:27:53 -0700

    Got DAC working

--- STM32/StdPeriph/Nucleo/F446RE/EmbeddedProject1/EmbeddedProject1.c
+++ STM32/StdPeriph/Nucleo/F446RE/EmbeddedProject1/EmbeddedProject1.c
@@ -3,7 +3,7 @@
 #include <stm32_hal_legacy.h>
 #include <stm32f4xx_dac.h>
 #include <stm32f4xx.h>
-
+#include <stm32f4xx_hal_rcc.h>
 #ifdef __cplusplus
 extern "C"
 #endif
@@ -15,15 +15,17 @@ void SysTick_Handler(void)
 
 int main(void)
 {
+	HAL_StatusTypeDef status;
 	GPIO_InitTypeDef GPIO_InitStructure;
-	DAC_InitTypeDef DAC_InitStructure;
-	DAC_HandleTypeDef DACH_Struc;
+	DAC_ChannelConfTypeDef sConfig;
+	DAC_HandleTypeDef hdac;
 	
 	HAL_Init();
 
 	__GPIOA_CLK_ENABLE();
-	//__DAC1_CLK_ENABLE();
-
+	
+	__HAL_RCC_DAC_CLK_ENABLE();
+	
 	GPIO_InitStructure.Pin = GPIO_PIN_4 | GPIO_PIN_5;
 	GPIO_InitStructure.Mode = GPIO_MODE_ANALOG;
 	GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
@@ -31,24 +33,26 @@ int main(void)
 	
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
-	DAC_InitStructure.DAC_Trigger = DAC_TRIGGER_NONE;
-	DAC_InitStructure.DAC_WaveGeneration = DAC_WAVEGENERATION_NONE;
-	DAC_InitStructure.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+	hdac.Instance = (DAC_TypeDef*)DAC1;
 	
-	DACH_Struc.Instance = (DAC_TypeDef*)DAC1;
-	HAL_DAC_Init(&DACH_Struc);
+	status = HAL_DAC_Init(&hdac);
 	
+	sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
+	sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_DISABLE;
 	
+	status = HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1);
+	status = HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_2);
+
+	uint32_t v = 0;
+	
+	status = HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+	status = HAL_DAC_Start(&hdac, DAC_CHANNEL_2);
+
 	for (;;)
 	{
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-		//HAL_Delay(5);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-		//HAL_Delay(5);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-		//HAL_Delay(5);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-		//HAL_Delay(5);
+		HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, v);
+		HAL_DAC_SetValue(&hdac, DAC_CHANNEL_2, DAC_ALIGN_12B_R, v);
+		v++;
 
 	}
 }
