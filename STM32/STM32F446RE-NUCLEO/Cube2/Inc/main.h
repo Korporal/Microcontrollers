cd3d8c5f97135e5980b783da24626c6af1aee4b3
Hugh <hugh_gleaves@hotmail.com>
2020-03-01 08:39:39 -0700

    Converted the actual DMA based Cube project downloaded from: https://controllerstech.com/dac-in-stm32/ To the GPDSC toolchain and reexported (to a temp folder) and tested. The manually copied/renamed and overwrote the various files in the Cube2 project folder. This now looks like the non-DMA version was modified to the working DMA version.

--- STM32/STM32F446RE-NUCLEO/Cube2/Inc/main.h
+++ STM32/STM32F446RE-NUCLEO/Cube2/Inc/main.h
@@ -7,7 +7,7 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under BSD 3-Clause license,
@@ -58,16 +58,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_13
-#define B1_GPIO_Port GPIOC
-#define LD2_Pin GPIO_PIN_5
-#define LD2_GPIO_Port GPIOA
-#define TMS_Pin GPIO_PIN_13
-#define TMS_GPIO_Port GPIOA
-#define TCK_Pin GPIO_PIN_14
-#define TCK_GPIO_Port GPIOA
-#define SWO_Pin GPIO_PIN_3
-#define SWO_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
